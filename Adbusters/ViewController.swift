30899125b8fb46291ddb161bc82c3d2d449f17a9
Kucherenko Sergey <buhanka1994@gmail.com>
2018-10-15 13:34:36 +0300

    Initial Commit

---
+++ Adbusters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adbusters
+//
+//  Created by MacBookAir on 10/15/18.
+//  Copyright © 2018 MacBookAir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
