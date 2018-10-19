b70e7f62dbce15ebd9b72775d9fce7204787df66
Kucherenko Sergey <buhanka1994@gmail.com>
2018-10-19 23:53:03 +0300

    Add adding photo from gallery

--- Adbusters/Controller/AddAdsViewController.swift
+++ Adbusters/Controller/AddAdsViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class AddAdsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class AddAdsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-    let images = [UIImage(named: "logo"), UIImage(named: "logo"), UIImage(named: "logo"), UIImage(named: "logo")]
+    @IBOutlet weak var collectionView: UICollectionView!
+    var images = [UIImage(named: "logo"), UIImage(named: "logo"), UIImage(named: "logo"), UIImage(named: "logo")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,41 @@ class AddAdsViewController: UIViewController, UICollectionViewDelegate, UICollec
     @IBAction func saveAd(_ sender: Any) {
     }
     
+    // Adding image
+    @IBAction func addAdImage(_ sender: Any) {
+        let imageController = UIImagePickerController()
+        imageController.delegate = self
+        imageController.allowsEditing = true
+
+        let actionSheet = UIAlertController(title: "Спосіб завантаження", message: "Виберіть спосіб завантаження", preferredStyle: .actionSheet)
+
+        actionSheet.addAction(UIAlertAction(title: "Камера", style: .default, handler: { (UIAlertAction) in
+            imageController.sourceType = .camera
+        }))
+
+        actionSheet.addAction(UIAlertAction(title: "Галерея", style: .default, handler: { (UIAlertAction) in
+            imageController.sourceType = .photoLibrary
+            self.present(imageController, animated: true, completion: nil)
+        }))
+
+        actionSheet.addAction(UIAlertAction(title: "Відмінити", style: .cancel, handler: nil))
+
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        images.append(image)
+        collectionView.reloadData()
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    // Collection view
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
