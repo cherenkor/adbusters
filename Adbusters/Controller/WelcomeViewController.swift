97eb766e53690fe46032f5ec7d822dc0640ad423
Kucherenko Sergey <buhanka1994@gmail.com>
2018-10-28 21:19:16 +0200

    Add instructions to the welcome screen

--- Adbusters/Controller/WelcomeViewController.swift
+++ Adbusters/Controller/WelcomeViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var instructionsView: UIView!
+    @IBOutlet weak var textField: UITextView!
+    
+    @IBAction func closeInstructions(_ sender: Any) {
+        instructionsView.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textField.textAlignment = .natural
     }
-    */
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
 }
